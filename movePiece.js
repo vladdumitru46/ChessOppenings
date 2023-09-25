864b590effb2e0b75acdf86b35653c42b94a51ce
vladdumitru46 <vladdumitru46@yahoo.com>
2023-09-25 17:51:13 +0300

    add

--- movePiece.js
+++ movePiece.js
@@ -1,9 +1,31 @@
+let square1 = null;
+let square2 = null;
+
 function getPiece(square) {
-    const id = square.id;
-    const piesa = square.getAttribute('data-piesa');
-    getInfoCasa(id, piesa);
+    if (square1 == null) {
+        square1 = square;
+    } else {
+        square2 = square;
+    }
+
+    if (square1 != null && square2 != null) {
+        alert(square1.querySelector('img').src +'\n' + square2.querySelector('img').src)
+        const piesaSquare1 = square1.getAttribute('data-piesa');
+        const imageUrlSquare1 = square1.querySelector('img').src;
+
+        square2.setAttribute('data-piesa', piesaSquare1);
+        square2.querySelector('img').src = imageUrlSquare1;
+
+        square1.setAttribute('data-piesa', 'none');
+        square1.querySelector('img').src = "";
+
+        square1 = null;
+        square2 = null;
+    }
 }
 
+
 function getInfoCasa(id, piesa) {
-    alert(`Casa ${id} contine piesa: ${piesa}`);
+    const message = `Casa ${id} conține piesa: ${piesa}`;
+    alert(message);
 }
