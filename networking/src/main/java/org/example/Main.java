64371b3c0ecdc37c9fbc7142c3772503c52a08db
vladdumitru46 <vladdumitru46@yahoo.com>
2024-01-19 17:02:43 +0200

    added check king logic and castling

--- networking/src/main/java/org/example/Main.java
+++ networking/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
+
+interface Foo {
+    boolean equals(Object arg0);
+
+}
+
